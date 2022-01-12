f97830e89bc31d11ed4dfb7b0becd994bdb5fd3c
Weichen Tie <75655299+WeichenTie@users.noreply.github.com>
2022-01-12 17:09:15 +1100

    added character creation screen

--- client/src/pages/CharacterCreation.js
+++ client/src/pages/CharacterCreation.js
@@ -6,55 +6,52 @@ import './CharacterCreation.css'
 import Avatar from '../components/Avatar';
 
 
-class CharacterCreation extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            col : 'black',
-        };
+
+const CharacterCreation = (props) => {
+    const [colourIndex, setColourIndex] = useState(0);
+    const [eyeIndex, setEyeIndex] = useState(0);
+    const [mouthIndex, setMouthIndex] = useState(0);
+
+    function incrementIndex (setter, newValue, max) {
+        if (newValue >= max) {
+            newValue = 0;
+        } else if (newValue < 0) {
+            newValue = max - 1;
+        }
+        setter(newValue);
     }
 
-    componentWillMount() {
-        console.log("HEYHEYHEY");
-    }
-    componentWillUnmount() {
-        console.log("BYE BYE");
-    }
-
-    render() {
-        this.a = 2;
-        //this.setState();
-        return (
-            <div className="page-container">
-                <div className="logo">
-                </div>
-                <div className="form-box">
-                    <input className="name-input-box" type="text" autoComplete="off" placeholder="Input name here"/>
-                    <div className="character-customization-box">
-                        <div className="arrow-group" id="left-arrow-group">
-                            <div className="arrow left-character-arrow"></div>
-                            <div className="arrow left-character-arrow"></div>
-                            <div className="arrow left-character-arrow"></div>
-                        </div>
-                        <div className="character-display">
-                            <Avatar/>
-                        </div>
-                        <div className="arrow-group" id="right-arrow-group">
-                            <div className="arrow right-character-arrow" style={{backgroundColor: `${this.state.col}`}}></div>
-                            <div className="arrow right-character-arrow"></div>
-                            <div className="arrow right-character-arrow"></div>
-                        </div>
-                    </div>
-                    <button className="create-character" onClick={() => {
-                        this.setState({col : 'purple'})}}>Create Character</button>
-                    <Link to={"/Lobby"}>
-                        <button className="create-character-button" type="submit">Create Character</button>
-                    </Link>
-                </div>
+    return (
+        <div className="page-container">
+            <div className="logo">
             </div>
-        );
-    }
+            <div className="form-box">
+                <input className="name-input-box" type="text" autoComplete="off" placeholder="Input name here"/>
+                <div className="character-customization-box">
+                    <div className="arrow-group" id="left-arrow-group">
+                        <div className="arrow arrow-left" onClick={() => {incrementIndex(setEyeIndex, eyeIndex - 1, 31)}}></div>
+                        <div className="arrow arrow-left" onClick={() => {incrementIndex(setMouthIndex, mouthIndex - 1, 24)}}></div>
+                        <div className="arrow arrow-left" onClick={() => {incrementIndex(setColourIndex, colourIndex - 1, 18)}}></div>
+                    </div>
+                    <Avatar
+                        size={96}
+                        colourIndex={colourIndex}
+                        mouthIndex={mouthIndex}
+                        eyeIndex={eyeIndex}>
+                    </Avatar>
+                    <div className="arrow-group" id="right-arrow-group">
+                        <div className="arrow arrow-right" onClick={() => {incrementIndex(setEyeIndex, eyeIndex + 1, 31)}}></div>
+                        <div className="arrow arrow-right" onClick={() => {incrementIndex(setMouthIndex, mouthIndex + 1, 24)}}></div>
+                        <div className="arrow arrow-right" onClick={() => {incrementIndex(setColourIndex, colourIndex + 1, 18)}}></div>
+                    </div>
+                </div>
+                <Link to={"/Lobby"}>
+                    <button className="create-character" type="submit">Create Character</button>
+                </Link>
+            </div>
+        </div>
+    );
 }
  
 export default CharacterCreation;
