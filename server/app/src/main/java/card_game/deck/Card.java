75f5a61da9b38952d80eb06e945e9acbeb7c3b1c
Weichen Tie <75655299+WeichenTie@users.noreply.github.com>
2022-01-12 18:18:57 +1100

    removed Java server

--- server/app/src/main/java/card_game/deck/Card.java
+++
@@ -1,9 +0,0 @@
-package card_game.deck;
-import java.util.List;
-
-import com.google.gson.*;
-
-public abstract class Card {
-    public abstract JsonObject toJsonObject();
-    public abstract List<? extends Card> loadCards();
-}
--- server/app/src/main/java/card_game/lobby/players/Player.java
+++
@@ -1,7 +0,0 @@
-package card_game.lobby.players;
-
-public class Player {
-    private String name;
-    private String id;
-    private String profile;
-}
